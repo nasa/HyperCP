Fix the satellite spectral weighting by eliminating all NaN columns in the Rrs and nLw.

ProcessL2.py
@@ -1126,7 +1126,16 @@ class ProcessL2:
             for k in rrsSlice:
                 newRrsData.columns[k].append(rrsSlice[k])
             for k in nLwSlice:
-                newnLwData.columns[k].append(nLwSlice[k])        
+                newnLwData.columns[k].append(nLwSlice[k])     
+
+        # Now clear out the columns in Rrs and nLw that have NaNs.        
+        delete = [key for key, value in newRrsData.columns.items() if np.isnan(value)]
+        for key in delete: del newRrsData.columns[key]
+        for key in delete: del newRrsDeltaData.columns[key]
+        delete = [key for key, value in newnLwData.columns.items() if np.isnan(value)]
+        for key in delete: del newnLwData.columns[key]
+        for key in delete: del newnLwDeltaData.columns[key]
+
 
         newESData.columnsToDataset()   
         newLIData.columnsToDataset()

ZhangRho.py
@@ -489,7 +489,7 @@ def Main(env, sensor):
     sensor['loc2'] = find_quads(*sensor['pol2'])
     
     # Probability and reflection angle of reflecting skylight into the sensor
-    ''' stop using loop until the efficiency is addressed '''
+    ''' Optionally stop using loop until the efficiency is addressed by saving and loading the result '''
     prob, angr_sky = get_prob(env['wind'], sensor['vec'])
     # np.save('prob.npy',prob)
     # np.save('angr_sky.npy',angr_sky)