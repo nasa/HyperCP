cc28bbfdf11e47678cc5367e4f82ad7084d3f457
dirk <dirk.a.aurin@nasa.gov>
2020-07-07 14:19:40 -0400

    Pyplot datetime Hack for mac

--- Source/test.py
+++ Source/test.py
@@ -1,40 +1,18 @@
-import os
-from PDFreport import PDF
+import pandas
+import dateutil.parser
+import datetime
+import matplotlib.pyplot as plt
 
-fileName = '1614106'
-pathOut = os.path.join('D:\\','Dirk','NASA','HyperPACE','Field_Data','HyperSAS','Processed','KORUS')
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
-dirPath = os.getcwd()
-inLogPath = os.path.join(dirPath, 'Logs')
-inPlotPath = os.path.join(pathOut,'Plots')
-outPDF = os.path.join(pathOut,'Reports', f'{fileName}.pdf')
-# title = f'File: {fileName} Collected: {root.attributes["TIME-STAMP"]}'
-title = f'File: {fileName} Collected: timestamp'
+start = dateutil.parser.parse("2018-05-17 19:00:00").replace(tzinfo=datetime.timezone.utc)
+end = dateutil.parser.parse("2018-05-17 20:00:00").replace(tzinfo=datetime.timezone.utc)
+df = pandas.DataFrame(data=[start, end], index=[0,1], columns=['x'])
+# *** HACK: CONVERT datetime column to string and back again - who knows why this works? ***
+df['x'] = pandas.to_datetime(df['x'].astype(str))
+print(df)
+print(df.dtypes)
 
-pdf = PDF()
-pdf.set_title(title)
-# pdf.set_author(f'HyperInSPACE_{MainConfig.settings["version"]}')
-pdf.set_author(f'HyperInSPACE_version')
-
-inLog = os.path.join(inLogPath,f'{fileName}_L1A.log')
-if os.path.isfile(inLog):
-    pdf.print_chapter('L1A', 'Process RAW to L1A', inLog, inPlotPath, fileName)
-inLog = os.path.join(inLogPath,f'{fileName}_L1A_L1B.log')
-if os.path.isfile(inLog):
-    pdf.print_chapter('L1B', 'Process L1A to L1B', inLog, inPlotPath, fileName)
-inLog = os.path.join(inLogPath,f'{fileName}_L1B_L1C.log')
-if os.path.isfile(inLog):
-    pdf.print_chapter('L1C', 'Process L1B to L1C', inLog, inPlotPath, fileName)
-inLog = os.path.join(inLogPath,f'{fileName}_L1C_L1D.log')
-if os.path.isfile(inLog):
-    pdf.print_chapter('L1D', 'Process L1C to L1D', inLog, inPlotPath, fileName)
-inLog = os.path.join(inLogPath,f'{fileName}_L1D_L1E.log')
-if os.path.isfile(inLog):
-    pdf.print_chapter('L1E', 'Process L1D to L1E', inLog, inPlotPath, fileName)
-inLog = os.path.join(inLogPath,f'{fileName}_L1E_L2.log')
-if os.path.isfile(inLog):
-    pdf.print_chapter('L2', 'Process L1E to L2', inLog, inPlotPath, fileName)
-
-pdf.output(outPDF, 'F')
-
-print('done')
+plt.plot(df['x'], [0,1], '--o')
+plt.show()
