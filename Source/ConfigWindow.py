f09aa479e8547127b90c09bae11071f09a24baf5
dirk <dirk.a.aurin@nasa.gov>
2023-04-19 09:00:06 -0400

    Add stylesheet to Main and ConfigWindow

--- Source/ConfigWindow.py
+++ Source/ConfigWindow.py
@@ -21,6 +21,7 @@ class ConfigWindow(QtWidgets.QDialog):
     ''' Configuration window object '''
     def __init__(self, name, inputDir, parent=None):
         super().__init__(parent)
+        self.setStyleSheet("background-color: #e3e6e1;")
         self.setModal(True)
         self.name = name
         self.inputDirectory = inputDir
