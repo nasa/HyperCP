nasa/HyperCP

commit 3048ccf4307121b4a766413c29231c484cb8cdc4

Add calibrationMap for TriOS. L1AQC for TriOS nearly complete

Source/Controller.py
@@ -2,10 +2,12 @@
 import os
 import datetime
 import numpy as np
+import collections
 
 from Source.HDFRoot import HDFRoot
 from Source.SeaBASSWriter import SeaBASSWriter
 from Source.CalibrationFileReader import CalibrationFileReader
+from Source.CalibrationFile import CalibrationFile
 from Source.MainConfig import MainConfig
 from Source.ConfigFile import ConfigFile
 from Source.Utilities import Utilities
@@ -227,16 +229,16 @@ class Controller:
                 cf.measMode = "Not Required"
                 cf.frameType = "Not Required"
                 cf.sensorType = cf.getSensorType()
-            else:
-                cf.instrumentType = "SAS"
-                cf.media = "Air"
-                cf.measMode = "VesselBorne"
-                cf.frameType = "LightAncCombined"
-                cf.sensorType = cf.getSensorType()
+            # else:
+            #     cf.instrumentType = "SAS"
+            #     cf.media = "Air"
+            #     cf.measMode = "VesselBorne"
+            #     cf.frameType = "LightAncCombined"
+            #     cf.sensorType = cf.getSensorType()
 
     @staticmethod
     def processCalibrationConfig(configName, calFiles):
-        ''' Read in calibration files/configuration '''
+        ''' Read in SeaBird calibration files/configuration '''
 
         # print("processCalibrationConfig")
         calFolder = os.path.splitext(configName)[0] + "_Calibration"
@@ -262,6 +264,32 @@ class Controller:
         return calibrationMap
 
     @staticmethod
+    def processCalibrationConfigTrios(calFiles):
+        ''' Write pseudo calibration/configuration map for TriOS'''
+
+        # print("processCalibrationConfig")
+        calibrationMap = collections.OrderedDict()
+
+        for key in list(calFiles.keys()):
+            cf = CalibrationFile()
+            print(key)
+            if '.ini' in key:
+                if calFiles[key]["enabled"]:
+                    cf.id = key
+                    cf.sensorType = calFiles[key]["frameType"]
+                    cf.name = key
+                    if calFiles[key]["frameType"] == 'ES':
+                        cf.instrumentType = "Reference"
+                    else:
+                        cf.instrumentType = "TriOS"
+                    cf.media = "Air"
+                    cf.measMode = "Surface"
+                    cf.frameType = "Combined"
+                    calibrationMap[key] = cf
+
+        return calibrationMap
+
+    @staticmethod
     def processAncData(fp):
         ''' Read in the ancillary field data file '''
 
@@ -271,6 +299,10 @@ class Controller:
             print("Specified ancillary file not found: " + fp)
             return None
         ancillaryData = AncillaryReader.readAncillary(fp)
+
+        if ConfigFile.settings['SensorType'].lower() == 'trios':
+            ancillaryData.columns['RELAZ'] = ancillaryData.columns['HOMEANGLE']
+            del ancillaryData.columns['HOMEANGLE']
         return ancillaryData
 
     @staticmethod
@@ -295,6 +327,7 @@ class Controller:
         # Write output file
         if root is not None:
             try:
+                # Throws AttributeError for each group...?
                 if flag_Trios:
                     root.writeHDF5(new_name)
                 else:

Source/TriosL1A.py
@@ -12,7 +12,10 @@ from Source.MainConfig import MainConfig
 from Source.HDFRoot import HDFRoot
 from Source.HDFGroup import HDFGroup
 
-
+'''
+All TriOS L0 (raw) and L1A data are in reverse chronological order.
+This will be correct in ... ?
+'''
 class TriosL1A:
 
     # use namesList to define dtype for recarray
@@ -144,7 +147,7 @@ class TriosL1A:
 
         # A = f.create_group('SAM_'+name+'.dat')
         gp =  HDFGroup()
-        gp.id = 'SAM_'+name+'.dat'
+        gp.id = 'SAM_'+name+'.ini'
         root.groups.append(gp)
 
         # Configuration file
@@ -182,7 +185,7 @@ class TriosL1A:
         rec_timetag2  = TriosL1A.reshape_data('NONE',len(meta[0]),data=timetag)
 
         # HDF5 Dataset creation
-        dataset_name = 'SAM_'+name+'.dat'
+        gp.attributes['CalFileName'] = 'SAM_'+name+'.ini'
         gp.addDataset('DATETAG')
         gp.datasets['DATETAG'].data=np.array(rec_datetag2, dtype=[('NONE', '<f8')])
         gp.addDataset('INTTIME')

Main.py
@@ -425,17 +425,17 @@ class Window(QtWidgets.QWidget):
             return
 
         print('Process Calibration Files')
-        filename = ConfigFile.filename
         calFiles = ConfigFile.settings['CalibrationFiles']
 
         if flag_Trios == 0:
-            calibrationMap = Controller.processCalibrationConfig(filename, calFiles)
-            if not calibrationMap.keys():
-                print('No calibration files found. '
-                'Check Config directory for your instrument files.')
-                return
+            calibrationMap = Controller.processCalibrationConfig(configFileName, calFiles)
         else:
-            calibrationMap = 0
+            calibrationMap = Controller.processCalibrationConfigTrios(calFiles)
+            # calibrationMap = 0
+        if not calibrationMap.keys():
+            print('No calibration files found. '
+            'Check Config directory for your instrument files.')
+            return
 
         print('Output Directory:', os.path.abspath(self.outputDirectory))
         if not self.outputDirectory[0]:

Source/AncillaryReader.py
@@ -1,6 +1,4 @@
 
-import csv
-
 from datetime import datetime
 import pytz
 
@@ -122,6 +120,10 @@ class AncillaryReader:
             if ds == "relaz": # Note: this misnomer is to trick readSB into accepting a non-conventional data field (home angle)
                 # SeaBASS thinks RelAz is between sensor and sun, but this is sensor to ship heading. We will call this dataset
                 # HOMEANGLE.
+                '''
+                This will need attention in the future when others may wish to report actual relAz in the Ancillary file
+                Reached out to SeaBASS to add a field for Sensor Azimuth.
+                '''
                 msg = f'Found data: {ds}'
                 print(msg)
                 Utilities.writeLogFile(msg)

Source/ProcessL1aqc.py
@@ -5,6 +5,7 @@ import numpy as np
 from pysolar.solar import get_azimuth, get_altitude
 from operator import add
 import bisect
+import glob, os
 
 from Source.HDFDataset import HDFDataset
 from Source.ProcessL1aqc_deglitch import ProcessL1aqc_deglitch
@@ -12,6 +13,45 @@ from Source.Utilities import Utilities
 from Source.ConfigFile import ConfigFile
 
 class ProcessL1aqc:
+
+    @staticmethod
+    def read_unc_coefficient(root, inpath):
+
+        # Read Uncertainties_new_char from provided files
+        gp = root.addGroup("RAW_UNCERTAINTIES")
+        gp.attributes['FrameType'] = 'NONE'  # add FrameType = None so grp passes a quality check later
+
+        ### Read uncertainty parameters from full calibration from TARTU
+        for f in glob.glob(os.path.join(inpath, r'pol/*')):
+            Utilities.read_char(f, gp)
+        for f in glob.glob(os.path.join(inpath, r'radcal/*')):
+            Utilities.read_char(f, gp)
+        for f in glob.glob(os.path.join(inpath, r'thermal/*')):
+            Utilities.read_char(f, gp)
+        for f in glob.glob(os.path.join(inpath, r'straylight/*')):
+            Utilities.read_char(f, gp)
+        for f in glob.glob(os.path.join(inpath, r'stability/*')):
+            Utilities.read_char(f, gp)
+        for f in glob.glob(os.path.join(inpath, r'linearity/*')):
+            Utilities.read_char(f, gp)
+        for f in glob.glob(os.path.join(inpath, r'angular/*')):
+            Utilities.read_char(f, gp)
+
+        ### unc dataset renaming
+        Utilities.RenameUncertainties(root)
+
+        ### interpolate unc to full wavelength range, depending on class based or full char
+        if ConfigFile.settings['bL1bCal'] == 2:
+            Utilities.interpUncertainties_DefaultChar(root)
+
+        elif ConfigFile.settings['bL1bCal'] == 3:
+            Utilities.interpUncertainties_FullChar(root)
+
+        ### generate temperature coefficient
+        Utilities.UncTempCorrection(root)
+
+        return root
+
 
     @staticmethod
     def filterData(group, badTimes):
@@ -70,28 +110,31 @@ class ProcessL1aqc:
 
         if gp.id.startswith("GPRMC") or gp.id.startswith("GPGAA"):
             gp.id = "GPS"
-        if gp.id.startswith("UMTWR"):
-            gp.id = "SOLARTRACKER_pySAS"
-        if gp.id.startswith("SATNAV"):
-            gp.id = "SOLARTRACKER"
-        if gp.id.startswith("SATMSG"):
-            gp.id = "SOLARTRACKER_STATUS"
-        if gp.id.startswith("SATPYR"):
-            gp.id = "PYROMETER"
-        if gp.id.startswith("HED"):
-            gp.id = "ES_DARK"
-        if gp.id.startswith("HSE"):
-            gp.id = "ES_LIGHT"
-        if gp.id.startswith("HLD"):
-            if cf.sensorType == "LI":
-                gp.id = "LI_DARK"
-            if cf.sensorType == "LT":
-                gp.id = "LT_DARK"
-        if gp.id.startswith("HSL"):
-            if cf.sensorType == "LI":
-                gp.id = "LI_LIGHT"
-            if cf.sensorType == "LT":
-                gp.id = "LT_LIGHT"
+        if ConfigFile.settings['SensorType'].lower == 'seabird':
+            if gp.id.startswith("UMTWR"):
+                gp.id = "SOLARTRACKER_pySAS"
+            if gp.id.startswith("SATNAV"):
+                gp.id = "SOLARTRACKER"
+            if gp.id.startswith("SATMSG"):
+                gp.id = "SOLARTRACKER_STATUS"
+            if gp.id.startswith("SATPYR"):
+                gp.id = "PYROMETER"
+            if gp.id.startswith("HED"):
+                gp.id = "ES_DARK"
+            if gp.id.startswith("HSE"):
+                gp.id = "ES_LIGHT"
+            if gp.id.startswith("HLD"):
+                if cf.sensorType == "LI":
+                    gp.id = "LI_DARK"
+                if cf.sensorType == "LT":
+                    gp.id = "LT_DARK"
+            if gp.id.startswith("HSL"):
+                if cf.sensorType == "LI":
+                    gp.id = "LI_LIGHT"
+                if cf.sensorType == "LT":
+                    gp.id = "LT_LIGHT"
+        else:
+            gp.id = cf.sensorType
 
     @staticmethod
     def processL1aqc(node, calibrationMap, ancillaryData=None):
@@ -124,9 +167,12 @@ class ProcessL1aqc:
         Utilities.writeLogFile(msg)
 
         # Reorganize groups in with new names
+        # if ConfigFile.settings['SensorType'].lower() == 'seabird':
         for gp in node.groups:
             cf = calibrationMap[gp.attributes["CalFileName"]]
             ProcessL1aqc.renameGroup(gp,cf)
+        # else:
+
 
         # Add a dataset to each group for DATETIME, as defined by TIMETAG2 and DATETAG
         node  = Utilities.rootAddDateTime(node)
@@ -146,7 +192,7 @@ class ProcessL1aqc:
                     gpsStatus = gp.getDataset('STATUS')
                 else:
                     gpsStatus = gp.getDataset('FIXQUAL')
-            elif gp.id.startswith('ES_LIGHT'):
+            elif gp.id.startswith('ES'):
                 esDateTime = gp.getDataset('DATETIME').data
             elif gp.id.startswith('SATTHS'):
                 # Fluxgate on the THS:
@@ -166,10 +212,8 @@ class ProcessL1aqc:
             # Remove all ancillary data that does not intersect GPS data
             # Change this to ES to work around set-ups with no GPS
             print('Removing non-pertinent ancillary data.')
-            # lower = bisect.bisect_left(ancDateTime, min(gpsDateTime))
             lower = bisect.bisect_left(ancDateTime, min(esDateTime))
             lower = list(range(0,lower-1))
-            # upper = bisect.bisect_right(ancDateTime, max(gpsDateTime))
             upper = bisect.bisect_right(ancDateTime, max(esDateTime))
             upper = list(range(upper,len(ancDateTime)))
             ancillaryData.colDeleteRow(upper)
@@ -195,13 +239,22 @@ class ProcessL1aqc:
             latAnc = ancillaryData.columns["LATITUDE"][0]
             lonAnc = ancillaryData.columns["LONGITUDE"][0]
 
-            homeAngle = None
+            relAzAnc = []
+            if "RELAZ" in ancillaryData.columns:
+                relAzAnc = ancillaryData.columns["RELAZ"][0]
+
+            sasAzimuth = []
+            # This is not a thing yet. Waiting for SeaBASS
+            if "SENSORAZ" in ancillaryData.columns:
+                sasAzimuth = ancillaryData.columns["SENSORAZ"][0]
+
+            homeAngle = []
             if "HEADING" in ancillaryData.columns:
                 # HEADING/shipAzimuth comes from ancillary data file here (not GPS or SATNAV)
                 # Or sasAzimuth comes from fluxgate SATTHS
                 shipAzimuth = ancillaryData.columns["HEADING"][0]
 
-                # HOMEANGLE in ANCILLARY is "RelAz", the angle between SAS and ship's bow
+                # HOMEANGLE in ANCILLARY the angle between SAS and ship's bow
                 if "HOMEANGLE" in ancillaryData.columns:
                     homeAngle = ancillaryData.columns["HOMEANGLE"][0]
                     for i, offset in enumerate(homeAngle):
@@ -216,7 +269,7 @@ class ProcessL1aqc:
                             print(msg)
                             Utilities.writeLogFile(msg)
                             sasAzimuth = compass.data['NONE'].tolist()
-                        else:
+                        elif not relAzAnc:
                             msg = 'Required ancillary sensor azimuth is missing. Abort.'
                             print(msg)
                             Utilities.writeLogFile(msg)
@@ -228,7 +281,7 @@ class ProcessL1aqc:
                         print(msg)
                         Utilities.writeLogFile(msg)
                         sasAzimuth = compass.data['NONE'].tolist()
-                    else:
+                    elif not relAzAnc:
                         msg = 'Required ancillary sensor azimuth is missing. Abort.'
                         print(msg)
                         Utilities.writeLogFile(msg)
@@ -292,6 +345,10 @@ class ProcessL1aqc:
             # Run Pysolar to obtain solar geometry.
             sunAzimuthAnc = []
             sunZenithAnc = []
+            # if not relAzAnc:
+            #     relAz = []
+            # else:
+            #     relAz = relAzAnc
             for i, dt_utc in enumerate(timeStamp):
                 sunAzimuthAnc.append(get_azimuth(latAnc[i],lonAnc[i],dt_utc,0))
                 sunZenithAnc.append(90 - get_altitude(latAnc[i],lonAnc[i],dt_utc,0))
@@ -627,24 +684,28 @@ class ProcessL1aqc:
                 print('ERROR: No sensor geometries found. Abort.')
                 return None
 
-        relAz=[]
-        for index in range(len(sunAzimuth)):
-            offset = home
+        # If relAz is already populated above, it is from the Ancillary file as actual RelAz
+        #   If not, try to use ancillary data to calculate it ()
+        if not relAzAnc:
+            for index in range(len(sunAzimuth)):
+                offset = home
 
-            # Check for angles spanning north
-            if sunAzimuth[index] > sasAzimuth[index]:
-                hiAng = sunAzimuth[index]
-                loAng = sasAzimuth[index] + offset
-            else:
-                hiAng = sasAzimuth[index] + offset
-                loAng = sunAzimuth[index]
-            # Choose the smallest angle between them. This will always be positive.
-            if hiAng-loAng > 180:
-                relAzimuthAngle = 360 - (hiAng-loAng)
-            else:
-                relAzimuthAngle = hiAng-loAng
+                # Check for angles spanning north
+                if sunAzimuth[index] > sasAzimuth[index]:
+                    hiAng = sunAzimuth[index]
+                    loAng = sasAzimuth[index] + offset
+                else:
+                    hiAng = sasAzimuth[index] + offset
+                    loAng = sunAzimuth[index]
+                # Choose the smallest angle between them. This will always be positive.
+                if hiAng-loAng > 180:
+                    relAzimuthAngle = 360 - (hiAng-loAng)
+                else:
+                    relAzimuthAngle = hiAng-loAng
 
-            relAz.append(relAzimuthAngle)
+                relAz.append(relAzimuthAngle)
+        else:
+            relAz = relAzAnc
 
         # In case there is no SolarTracker to provide sun/sensor geometries, Pysolar was used
         # to estimate sun zenith and azimuth using GPS position and time, and sensor azimuth will
@@ -843,7 +904,10 @@ class ProcessL1aqc:
 
         ###########################################################################
         # Now deglitch
-        node = ProcessL1aqc_deglitch.processL1aqc_deglitch(node)
+        if ConfigFile.settings["SensorType"] == "Seabird":
+            node = ProcessL1aqc_deglitch.processL1aqc_deglitch(node)
+        else:
+            node.attributes['L1AQC_DEGLITCH'] = 'OFF'
 
         # DATETIME is not supported in HDF5; remove
         if node is not None:

commit 42e896a84705262865c3e07d07f92bf7ad1b6dac

Fix TriOS BACK_ CAL_ datasets. Fix Controller bug introduced in 3048ccf.

Config/sample_NOTRACKER.hdr
@@ -1,0 +1,1 @@
+{"comments": "! HyperInSPACE vers = 1.2.0\n! HyperInSPACE Config = sample_TRIOS_NOTRACKER.cfg\n! SZA Filter = On\n! SZA Max = 70.0\n! Rotator Home Angle = 0.0\n! Rotator Delay = 60.0\n! Pitch/Roll Filter = Off\n! Max Pitch/Roll = 5.0\n! Rotator Min/Max Filter = Off\n! Rotator Min = -60.0\n! Rotator Max = 15.0\n! Rel Azimuth Filter = On\n! Rel Azimuth Min = 90.0\n! Rel Azimuth Max = 135.0\n! Deglitch Filter = Off\n! ES Dark Window = 11\n! ES Light Window = 5\n! ES Dark Sigma = 3.2\n! ES Light Sigma = 3.5\n! LI Dark Window = 11\n! LI Light Window = 5\n! LI Dark Sigma = 3.2\n! LI Light Sigma = 3.4\n! LT Dark Window = 11\n! LT Light Window = 5\n! LT Dark Sigma = 3.0\n! LT Light Sigma = 3.2\n! Wavelength Interp Int = 3.3\n! Default Wind = 5.0\n! Default AOD = 0.5\n! Default Salt = 35.0\n! Default SST = 26.0\n! Max Wind = 10.0\n! Min SZA = 20.0\n! Max SZA = 60.0\n! Spectral Filter = On\n! Filter Sigma Es = 5.0\n! Filter Sigma Li = 8.0\n! Filter Sigma Lt = 3.0\n! Meteorological Filter = On\n! Cloud Flag = 1.0\n! Es Flag = 2.0\n! Dawn/Dusk Flag = 1.0\n! Rain/Humidity Flag = 1.095\n! Ensemble Interval = 300\n! Percent Lt Filter = On\n! Percent Light = 10.0\n! Glint_Correction = Mobley 1999\n! NIR Correction = Mueller and Austin 1995\n! Remove Negatives = On"}

Source/Controller.py
@@ -229,12 +229,12 @@ class Controller:
                 cf.measMode = "Not Required"
                 cf.frameType = "Not Required"
                 cf.sensorType = cf.getSensorType()
-            # else:
-            #     cf.instrumentType = "SAS"
-            #     cf.media = "Air"
-            #     cf.measMode = "VesselBorne"
-            #     cf.frameType = "LightAncCombined"
-            #     cf.sensorType = cf.getSensorType()
+            else:
+                cf.instrumentType = "SAS"
+                cf.media = "Air"
+                cf.measMode = "VesselBorne"
+                cf.frameType = "LightAncCombined"
+                cf.sensorType = cf.getSensorType()
 
     @staticmethod
     def processCalibrationConfig(configName, calFiles):

Source/HDFDataset.py
@@ -89,18 +89,6 @@ class HDFDataset:
             #print("type",type(ltData.data[k]))
             self.columns[k] = self.data[k].tolist()
 
-    def datasetToColumns2(self):
-        ''' Convert Prosoft format numpy array to columns '''
-        if self.data is None:
-            print("Warning - datasetToColumns2: data is empty")
-            return
-        self.columns = collections.OrderedDict()
-        ids = self.attributes["ID"]
-        for k in ids:
-            self.columns[k] = []
-        for k in ids:
-            self.columns[k].append(self.data[0][ids.index(k)])
-
     def columnsToDataset(self):
         ''' Converts columns into numpy array '''
         #dtype0 = np.dtype([(name, type(ds.columns[name][0])) for name in ds.columns.keys()])

Source/HDFRoot.py
@@ -1,6 +1,5 @@
 
 import collections
-import sys
 import h5py
 import numpy as np
 

Source/TriosL1A.py
@@ -2,7 +2,7 @@
 import datetime as dt
 import numpy as np
 import pandas as pd
-import h5py
+# import h5py
 import json
 from datetime import timedelta, date
 import re
@@ -13,10 +13,7 @@ from Source.HDFRoot import HDFRoot
 from Source.HDFGroup import HDFGroup
 from Source.Utilities import Utilities
 
-'''
-All TriOS L0 (raw) and L1A data are in reverse chronological order.
-This will be correct in ... ?
-'''
+
 class TriosL1A:
 
     # use namesList to define dtype for recarray
@@ -26,12 +23,12 @@ class TriosL1A:
         rec_arr2 = np.rec.fromarrays(tst, dtype=ds_dt)
         return rec_arr2
 
-    def reshape_data_str(NAME,N,data):
-        dt = h5py.special_dtype(vlen=str)
-        ds_dt = np.dtype({'names':[NAME],'formats':[dt] })
-        tst = np.array(data).reshape((1,N))
-        rec_arr2 = np.rec.fromarrays(tst, dtype=ds_dt)
-        return rec_arr2
+    # def reshape_data_str(NAME,N,data):
+    #     dt = h5py.special_dtype(vlen=str)
+    #     ds_dt = np.dtype({'names':[NAME],'formats':[dt] })
+    #     tst = np.array(data).reshape((1,N))
+    #     rec_arr2 = np.rec.fromarrays(tst, dtype=ds_dt)
+    #     return rec_arr2
 
     # Function for reading and formatting .dat data file
     def read_dat(inputfile):
@@ -227,7 +224,8 @@ class TriosL1A:
         rec_arr = np.rec.fromarrays(my_arr, dtype=ds_dt)
         # gp.addDataset(''+sensor,data=rec_arr)
         gp.addDataset(sensor)
-        gp.datasets[sensor].data=rec_arr
+        # gp.datasets[sensor].data=rec_arr
+        gp.datasets[sensor].data=np.array(rec_arr, dtype=ds_dt)
 
 
         # Calibrations files
@@ -240,7 +238,8 @@ class TriosL1A:
         metaback,back = TriosL1A.read_cal(cal_path + 'Back_SAM_'+name+'.dat')
         # C1 = gp.addDataset('BACK_'+sensor,data=back[[1,2]].astype(np.float64))
         C1 = gp.addDataset('BACK_'+sensor)
-        C1.data = back[[1,2]].astype(np.float64)
+        # C1.data = back[[1,2]].astype(np.float64)
+        C1.data = np.array(back[[1,2]].astype(np.float64))
         TriosL1A.get_attr(metaback,C1)
 
         start_time = dt.datetime.strftime(dt.datetime(1900,1,1) + timedelta(days=rec_datetag[0][0]-2), "%Y%m%dT%H%M%SZ")
@@ -260,7 +259,11 @@ class TriosL1A:
                 dateTime.append(Utilities.timeTag2ToDateTime(dt,timeTagArray[i][0]))
 
             for ds in gp.datasets:
-                gp.datasets[ds].data = np.array([x for _, x in sorted(zip(dateTime,gp.datasets[ds].data))])
+
+                # BACK_ and CAL_ are nLambda x 2 and nLambda x 1, respectively, not timestamped to DATETAG, TIMETAG2
+                if (not ds.startswith('BACK_')) and (not ds.startswith('CAL_')):
+                    gp.datasets[ds].datasetToColumns()
+                    gp.datasets[ds].data = np.array([x for _, x in sorted(zip(dateTime,gp.datasets[ds].data))])
 
         return node
 

Source/TriosL1B.py
@@ -143,7 +143,8 @@ class TriosL1B:
         # return the wvl of dark pixel for which no calibration factor is defined
 
         # Read HDF file inputs
-        grp = node.getGroup(instrument_number+'.dat')
+        # grp = node.getGroup(instrument_number+'.dat')
+        grp = node.getGroup(sensortype)
         raw_cal  = grp.getDataset("CAL_"+sensortype).data
         raw_back = grp.getDataset("BACK_"+sensortype).data
         raw_data = np.asarray(grp.getDataset(sensortype).data.tolist())
@@ -151,7 +152,8 @@ class TriosL1B:
         int_time = np.asarray(grp.getDataset("INTTIME").data.tolist())
         DarkPixelStart = int(grp.attributes["DarkPixelStart"])
         DarkPixelStop  = int(grp.attributes["DarkPixelStop"])
-        int_time_t0 = int(grp.getDataset("BACK_"+sensortype).attributes["IntegrationTime"])
+        # int_time_t0 = int(grp.getDataset("BACK_"+sensortype).attributes["IntegrationTime"])
+        int_time_t0 = int(grp.attributes["IntegrationTime"])
 
         # check size of data
         nband = len(raw_back[:,0])
@@ -227,12 +229,12 @@ class TriosL1B:
         timestr = now.strftime("%d-%b-%Y %H:%M:%S")
         node.attributes["FILE_CREATION_TIME"] = timestr
         node.attributes['WAVE_INTERP'] = str(ConfigFile.settings['fL1bInterpInterval']) + ' nm'
-        if ConfigFile.settings["bL1bDefaultCal"] == 1:
+        if ConfigFile.settings["bL1bCal"] == 1:
             node.attributes['CAL_TYPE'] = 'Factory'
-        elif ConfigFile.settings["bL1bDefaultCal"] == 2:
+        elif ConfigFile.settings["bL1bCal"] == 2:
             node.attributes['CAL_TYPE'] = 'Class-based'
-        elif ConfigFile.settings["bL1bDefaultCal"] == 3:
-            node.attributes['CAL_TYPE'] = 'Full Characterisation'
+        elif ConfigFile.settings["bL1bCal"] == 3:
+            node.attributes['CAL_TYPE'] = 'Instrument-based'
 
         msg = f"TriosL1B.processL1b: {timestr}"
         print(msg)
@@ -262,13 +264,13 @@ class TriosL1B:
                 print(msg)
                 Utilities.writeLogFile(msg)
 
-                if ConfigFile.settings["bL1bDefaultCal"] <= 2:
+                if ConfigFile.settings["bL1bCal"] <= 2:
                     if not TriosL1B.processDarkCorrection(node, instrument_number, sensortype, stats):
                         msg = f'Error in TriosL1B.processDarkCorrection: {instrument_number} - {sensortype}'
                         print(msg)
                         Utilities.writeLogFile(msg)
                         return None
-                elif ConfigFile.settings['bL1bDefaultCal'] == 3:
+                elif ConfigFile.settings['bL1bCal'] == 3:
                     if not TriosL1B.processDarkCorrection_FRM(node, instrument_number, sensortype, stats):
                         msg = f'Error in TriosL1B.processDarkCorrection_FRM: {instrument_number} - {sensortype}'
                         print(msg)
@@ -277,19 +279,19 @@ class TriosL1B:
 
 
         ## Uncertainty computation/initialisation
-        if ConfigFile.settings["bL1bDefaultCal"] == 1:
+        if ConfigFile.settings["bL1bCal"] == 1:
             if not ProcessL1b.process_Default_Uncertainties(node, stats):
                 msg = 'Error in process_Default_Uncertainties'
                 print(msg)
                 Utilities.writeLogFile(msg)
                 return None
-        elif ConfigFile.settings["bL1bDefaultCal"] == 2:
+        elif ConfigFile.settings["bL1bCal"] == 2:
             if not ProcessL1b.process_Class_Uncertainties(node, stats):
                 msg = 'Error in process_Class_Uncertainties'
                 print(msg)
                 Utilities.writeLogFile(msg)
                 return None
-        elif ConfigFile.settings['bL1bDefaultCal'] == 3:
+        elif ConfigFile.settings['bL1bCal'] == 3:
             if not ProcessL1b.process_FRM_Uncertainties(node, stats):
                 msg = 'Error in process_FRM_Uncertainties'
                 print(msg)