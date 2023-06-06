Fix timezone in GetAnc_ecmwf.py and add units/wavebands to ancillary model attributes

Source/GetAnc_ecmwf.py
@@ -79,7 +79,7 @@ class GetAnc_ecmwf:
         latLonTag: a string, a tag to indicate the effective lat/lon.
         dateTag: a string, a tag to indicate time stamp, to the hour
         '''
-        
+
         # Lat-Lon
         latEff = np.round(lat / latRes) * latRes
         lonEff = np.round(lon / lonRes) * lonRes
@@ -97,7 +97,7 @@ class GetAnc_ecmwf:
         # Time
         year, month, day, hour, minute, second = GetAnc_ecmwf.timeStamp2yrMnthDayHrMinSec(timeStamp)
 
-        timeSec = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second)).timestamp()
+        timeSec = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second),tzinfo=datetime.timezone.utc).timestamp()
 
         timeEffSec = np.round(timeSec / (3600 * timeResHours)) * (3600 * timeResHours) - 7200
 
@@ -272,6 +272,8 @@ class GetAnc_ecmwf:
         modGroup.datasets['Timetag2'] = latTime
         modGroup.datasets['AOD'] = modAOD
         modGroup.datasets['Wind'] = modWind
+        modGroup.attributes['Wind units'] = 'm s-1'
+        modGroup.attributes['AOD wavelength'] = '550 nm'
         print('GetAnc_ecmwf: Model data retrieved')
 
         return modData

Source/ProcessL1b.py
@@ -185,7 +185,7 @@ class ProcessL1b:
                     wind[i] = modRoot.groups[0].datasets['Wind'][idx]
                     windFlag[i] = 'model'
                     if i==0:
-                        aodDataset.attributes['Model Wind units'] = modRoot.groups[0].attributes['Wind units']
+                        ancGroup.attributes['Model Wind units'] = modRoot.groups[0].attributes['Wind units']
                 if np.isnan(aod[i]):
                     # msg = 'Replacing AOD with model data'
                     # print(msg)
@@ -194,7 +194,8 @@ class ProcessL1b:
                     aod[i] = modRoot.groups[0].datasets['AOD'][idx]
                     aodFlag[i] = 'model'
                     if i==0:
-                        aodDataset.attributes['Model AOD wavelength'] = modRoot.groups[0].attributes['AOD wavelength']
+                        # aodDataset.attributes['Model AOD wavelength'] = modRoot.groups[0].attributes['AOD wavelength']
+                        ancGroup.attributes['Model AOD wavelength'] = modRoot.groups[0].attributes['AOD wavelength']
 
         # Replace Wind, AOD, SST, and Sal with defaults where still nan
         msg = 'Filling in ancillary data with default values where still needed.'
@@ -572,8 +573,6 @@ class ProcessL1b:
             modRoot = None
 
         if modRoot is not None:
-            # return None
-
             # Regardless of whether SolarTracker/pySAS is used, Ancillary data will have been already been
             # interpolated in L1B as long as the ancillary file was read in at L1AQC. Regardless, these need
             # to have model data and/or default values incorporated.