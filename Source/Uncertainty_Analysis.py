30124bb59a02327ca4d5a4fe2827c985581d41cb
Dirk Aurin <dirk.a.aurin@nasa.gov>
2025-06-24 14:42:07 -0400

    Block usage of Z17 LUTs

--- Source/Uncertainty_Analysis.py
+++ Source/Uncertainty_Analysis.py
@@ -395,11 +395,11 @@ class Propagate:
             sza = 60
 
         from Source.RhoCorrections import InterpolationError
-        try:
-            zhang = RhoCorrections.read_Z17_LUT(windSpeedMean, AOD, sza, wTemp - 273.15, sal, relAz, sva, waveBands)
-        except InterpolationError as err:
-            Utilities.writeLogFileAndPrint(f'{err}: Unable to use LUT interpolations. Reverting to analytical solution.')
-            zhang = Propagate.zhangWrapper(windSpeedMean, AOD, sza, wTemp - 273.15, sal, relAz, sva, waveBands)
+        # try:
+        #     zhang = RhoCorrections.read_Z17_LUT(windSpeedMean, AOD, sza, wTemp - 273.15, sal, relAz, sva, waveBands)
+        # except InterpolationError as err:
+            # Utilities.writeLogFileAndPrint(f'{err}: Unable to use LUT interpolations. Reverting to analytical solution.')
+        zhang = Propagate.zhangWrapper(windSpeedMean, AOD, sza, wTemp - 273.15, sal, relAz, sva, waveBands)
         
         return zhang
 
