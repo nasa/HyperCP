68a009b42a81a32f0a3a4c002358695a1d46365b
Dirk Aurin <dirk.a.aurin@nasa.gov>
2025-10-22 11:15:14 -0400

    Change thermalling for sensor list per #393 comments

--- Source/utils/thermalling.py
+++ Source/utils/thermalling.py
@@ -17,8 +17,12 @@ def UncTempCorrection(node):
 
     unc_grp = node.getGroup("RAW_UNCERTAINTIES")
     # sensorID = Utilities.get_sensor_dict(node)
-    # inv_ID = {v: k for k, v in sensorID.items()}
-    sensor_list = [g.id for g in node.groups if g.id in ('ES', 'LI', 'LT')]
+    # inv_ID = {v: k for k, v in sensorID.items()}    
+    sensor_list = []
+    for grp in node.groups:
+        for s in ['ES', 'LI', 'LT']:
+            if s in grp.id and not s in sensor_list:
+                sensor_list.append(s)
     for sensor in sensor_list:
         TempCoeffDS = unc_grp.getDataset(sensor+"_TEMPDATA_CAL")
 
