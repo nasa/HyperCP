738ffa5b5def563f3bd91a855b941feca77cff84
ARamsay17 <ashley.ramsay@npl.co.uk>
2026-04-15 15:00:59 +0100

    unittest direcotry and files added

---
+++ Source/PIU/tests/test_BaseInstrument.py
@@ -0,0 +1,31 @@
+import unittest
+from Source.PIU import BaseInstrument
+
+
+class test_baseInstrument(unittest.TestCase):
+    def test_generate_sensor_stats(self):
+        pass
+
+    def test_classBasedL1A(self):
+        pass
+
+    def test_classBasedL2(self):
+        pass
+
+    def test_ClassBasedL2ESOnly(self):
+        pass
+
+    def test_FRML2(self):
+        pass
+
+    def test_band_conv(self):
+        # CB case
+
+        # SB case
+        
+        pass
+
+    def test_interpolation(self):
+        # interp_and_slice_raw_data
+        # get_interp_data
+        pass
