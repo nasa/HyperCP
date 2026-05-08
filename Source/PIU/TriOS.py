875401df7cc73a55e40f1ce7f55113cd56a10bf9
ARamsay17 <ashley.ramsay@npl.co.uk>
2026-05-08 10:12:15 +0100

    normalisation of lightDarkStats for TriOS

--- Source/PIU/TriOS.py
+++ Source/PIU/TriOS.py
@@ -64,28 +64,31 @@ class TriOS(BaseInstrument):
         back_mesure = np.zeros((nmes, nband))
         back_corrected_mesure   = np.zeros((nmes, nband))
         offset_corrected_mesure = np.zeros((nmes, nband))
+        offset = np.zeros(nmes)
+        normalised_mesure = np.zeros((nmes, nband))
+        Light = np.zeros((nmes, nband))
 
-        offset = []
         for n in range(nmes):
             # std(back) + std(raw) add in quad  - what about normalise and cal?
             # Background correction : B0 and B1 read from "back data"
-            back_mesure[n, :] = raw_back[:, 0] + raw_back[:, 1]*(int_time[n]/int_time_t0)
+            back_mesure[n, :] = raw_back[:, 0] + (raw_back[:, 1]*(int_time[n]/int_time_t0))
             back_corrected_mesure[n, :] = mesure[n] - back_mesure[n, :]
 
             # Offset substraction : dark index read from attribute
-            offset.append(np.mean(back_corrected_mesure[n, DarkPixelStart:DarkPixelStop]))
-            offset_corrected_mesure[n, :] = back_corrected_mesure[n] - offset[-1]
+            offset[n] = np.mean(back_corrected_mesure[n, DarkPixelStart:DarkPixelStop])
+            offset_corrected_mesure[n, :] = back_corrected_mesure[n] - offset[n]
 
             # Normalization for integration time
-            # normalized_mesure = offset_corrected_mesure*int_time_t0/int_time[n]
+            normalised_mesure[n, :] = offset_corrected_mesure[n, :]*int_time_t0/int_time[n]
+            Light[n, :] = back_corrected_mesure[n, :]*int_time_t0/int_time[n]
 
             # Sensitivity calibration
             # calibrated_mesure[n, :] = normalized_mesure/raw_cal  # uncommented /raw_cal L1985-6
 
         # get light and dark data before correction
-        light_avg = np.mean(back_corrected_mesure, axis=0)  # [ind_nocal == False]
+        light_avg = np.mean(Light, axis=0)  # [ind_nocal == False]
         if nmes > 25:
-            light_std = np.std(back_corrected_mesure, axis=0) / pow(nmes, 0.5)  # [ind_nocal == False]
+            light_std = np.std(Light, axis=0) / pow(nmes, 0.5)  # [ind_nocal == False]
         elif nmes > 3:
             light_std = np.sqrt(((nmes-1)/(nmes-3))*(np.std(back_corrected_mesure, axis=0) / np.sqrt(nmes))**2)
         else:
@@ -95,18 +98,19 @@ class TriOS(BaseInstrument):
         ones = np.ones(nband)  # to provide array of 1s with the correct shape
         dark_avg = ones * np.mean(offset)
         if nmes > 25:
-            dark_std = ones * (np.std(np.mean(back_corrected_mesure[:, DarkPixelStart:DarkPixelStop], axis=1), axis=0) / pow(nmes, 0.5))
+            dark_std = ones * (np.std(np.mean(Light[:, DarkPixelStart:DarkPixelStop], axis=1), axis=0) / pow(nmes, 0.5))
         else:  # already checked for light data so we know nmes > 3
             dark_std = np.sqrt(((nmes-1)/(nmes-3))*
-            (ones * (np.std(np.mean(back_corrected_mesure[:, DarkPixelStart:DarkPixelStop], axis=1), axis=0)/np.sqrt(nmes)**2)))
+            (ones * (np.std(np.mean(Light[:, DarkPixelStart:DarkPixelStop], axis=1), axis=0)/np.sqrt(nmes)**2)))
             # adjusting the dark_ave and dark_std shapes will remove sensor specific behaviour in Default and Factory
 
+        # both are relative to the normalised dark corrected signal
         signal_noise = {}
         for i, wvl in enumerate(raw_wvl):
             signal_noise[wvl] = pow(
-                (pow(light_std[i], 2) + pow(dark_std[i], 2)) / pow(np.average(offset_corrected_mesure, axis=0)[i], 2), 0.5)  # sqrt(sigma_light^2 + sigma_dark^2 / dark_corrected_signal^2)
+                (pow(light_std[i], 2) + pow(dark_std[i], 2)) / pow(np.average(normalised_mesure, axis=0)[i], 2), 0.5)  # sqrt(sigma_light^2 + sigma_dark^2 / dark_corrected_signal^2)
 
-        std_signal = np.std(offset_corrected_mesure, axis=0) / np.average(offset_corrected_mesure, axis=0)  # this is relative
+        std_signal = np.std(normalised_mesure, axis=0) / np.average(normalised_mesure, axis=0)  # this is relative
 
         return dict(
             ave_Light=np.array(light_avg),
